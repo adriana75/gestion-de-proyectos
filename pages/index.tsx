d1377b9438cfd3bc381710435a8d33a2cf2f06ba
adriana75 <tecnologianormal@hotmail.com>
2021-11-09 08:42:28 -0500

    instalación de tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,15 @@
 import type { NextPage } from 'next'
+import Link from 'next/link'
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <div className='bg-green-500'>
       Hola mundo
+
+      <p>Página Index</p>
+      <Link href='/admin/usuarios'>
+        <a className='cursor-pointer'>Ir a admin de usuarios</a>
+      </Link>
     </div>
   )
 }
